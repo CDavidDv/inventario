93244bbdb59ec062730562bfecb5101be338705d
CDavidDv <carlos.david1806@gmail.com>
2025-10-16 12:31:03 -0600

    Refactor asset loading in app.blade.php to use manifest for production environment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,8 +18,17 @@
             <script type="module" src="http://localhost:5173/resources/js/app.js"></script>
             <link rel="stylesheet" href="http://localhost:5173/resources/css/app.css">
         @else
-            <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-            <script src="{{ asset('build/assets/app.js') }}" defer></script>
+            @php
+                $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+                $cssFile = $manifest['resources/css/app.css']['file'] ?? 'assets/app.css';
+                $jsFile = $manifest['resources/js/app.js']['file'] ?? 'assets/app.js';
+                $appCss = $manifest['resources/js/app.js']['css'] ?? [];
+            @endphp
+            <link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
+            @foreach($appCss as $css)
+                <link rel="stylesheet" href="{{ asset('build/' . $css) }}">
+            @endforeach
+            <script type="module" src="{{ asset('build/' . $jsFile) }}"></script>
         @endif
         @inertiaHead
     </head>
