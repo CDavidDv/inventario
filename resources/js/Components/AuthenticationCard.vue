676488339987454ace63e549003f5782650ddd65
CDavidDv <carlos.david1806@gmail.com>
2025-10-24 13:54:33 -0600

    fix: ajustar el espaciado en AuthenticationCard y agregar favicon en app.blade.php

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex flex-col justify-center px-4 items-center pb-6 sm:pt-0 bg-white">
+    <div class="min-h-screen flex flex-col pt-32 sm:justify-center px-4 items-center pb-6 sm:pt-0 bg-white">
         
         <div class="w-full  sm:max-w-sm mt-6 px-6 pb-10 pt-6 bg-[#80aa40] shadow-2xl overflow-hidden sm:rounded-4xl  rounded-2xl">
             <slot />
